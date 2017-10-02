1a030e62f0a8ca0c77003ec13843f192964978d7
Nikki Roberts <robertsni@s.dcsdk12.org>
2017-10-02 10:45:06 -0600

    implement has_teen

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,14 @@
   
 # TODO - write has_teen?
+def has_teen?(age)
 
+	if age == 13, 14, 15, 16, 17, 18, 19
+		return true
+	elsif age != 13, 14, 15, 16, 17, 18, 19
+		return false
+	end
+	
+end
 # TODO - write not_string
 
 # TODO - write icy_hot?
