3ddd9a6f23b04a688fe93a23ee22b268c7406f49
Nikki Roberts <robertsni@s.dcsdk12.org>
2017-10-02 10:40:05 -0600

    tests for has_teen?

--- test.rb
+++ test.rb
@@ -2,4 +2,20 @@ require './test_setup'
 require './methods_quiz'
 
 # TODO - write tests here
+describe 'methods-quiz' do 
 
+	describe 'has_teen?'
+		it '12 is not a teen' do
+			has_teen?(12).must_equal(false)
+		end
+
+		it '16 is a teen' do
+			has_teen(16).must_equal(true)
+		end
+
+		it 'abc is invalid input' do
+			has_teen("abc").must_equal(false)
+		end
+	end
+	
+end
